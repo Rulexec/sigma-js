63981db3e7df4b904e2f84e778fd9e7f756211a8
Alexander Ruliov <alexander.ruliov@gmail.com>
2014-07-16 21:53:35 +0400

    dependent types fixed

--- tests/dependent.js
+++ tests/dependent.js
@@ -65,4 +65,56 @@ describe('dependent types', function() {
       value: {s: 'some message'}
     });
   });
+
+  it('union-typed specific case', function() {
+    var schemas = {
+      type: 'schemas',
+      schemas: {
+        some: {
+          type: 'dependent',
+          parameters: {t: {type: 'union', variants: {foo: {type: 'product', components: {}}}}},
+          cases: function(parameters) {
+            switch (parameters.t.name) {
+            case 'foo':
+              return {
+                type: 'product',
+                components: {
+                  a: 'uint64',
+                  b: 'uint32',
+                  args: {
+                    type: 'product',
+                    components: {
+                      c: 'uint64'
+                    }
+                  }
+                }
+              };
+            default: throw new Error("Unknown parameter: " + fullInspect(parameters.t));
+            }
+          }
+        }
+      }
+    };
+
+    var expectedData = {
+      name: 'some',
+      value: {
+        parameters: {
+          t: 'foo'
+        },
+        value: {
+          a: '0',
+          b: 50,
+          args: {
+            c: '42'
+          }
+        }
+      }
+    };
+
+    var actualData = serializer.validateAndNormalize(expectedData, schemas);
+
+    expectedData.value.parameters.t = {name: 'foo', value: {}};
+    assert.deepEqual(actualData, expectedData);
+  });
 });
